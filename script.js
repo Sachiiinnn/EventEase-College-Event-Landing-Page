86f4a869d5db809bc6dedd6076c0d956926bd8f3
Sachiiinnn <sanshu9582@gmail.com>
2025-07-23 21:38:13 +0530

    added responsiveness

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const searchForm = document.querySelector('.searchForm');
 
 document.querySelector('#searchButton').onclick = () =>{
     searchForm.classList.toggle("active");
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
 }
 
 const formButton = document.querySelector('#loginButton');
@@ -10,8 +12,22 @@ const formClose = document.querySelector('#formClose');
 
 formButton.addEventListener('click', () =>{
     loginForm.classList.add("active");
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+    searchForm.classList.remove("active")
 });
 
 formClose.addEventListener('click', () =>{
     loginForm.classList.remove("active")
 })
+
+
+
+let menu = document.querySelector('#menuButton');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+    searchForm.classList.remove("active");
+}
